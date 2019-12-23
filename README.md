17270a750aab509caa6c8fa4aa8b42457bf221fc
EvilScott <reis.robert.s@gmail.com>
2019-12-23 15:46:46 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# boardgames
+populate `data/` from https://www.kaggle.com/mseinstein/bgg_top2000
---
+++ src/App.svelte
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1>boardgames</h1>
+    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</div>
