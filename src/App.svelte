3928848ddc6d29b3c94f1e05b64c9fc55ce4847f
EvilScott <reis.robert.s@gmail.com>
2019-12-23 22:30:55 -0800

    add filters to the top of the page

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,29 @@
 <script>
     import BoardGame from '~/components/BoardGame.svelte';
-    import { filteredList } from '~/utils/stores.js';
+    import Designer from '~/components/Designer.svelte';
+    import Tag from '~/components/Tag.svelte';
+    import { filters, filteredList } from '~/utils/stores.js';
 </script>
 
 <div class="container">
     <h1>boardgames</h1>
+    <div class="filters">
+    {#each $filters.designers as name}
+        <Designer name={name} />
+    {/each}
+    {#each $filters.tags as name}
+        <Tag name={name} />
+    {/each}
+    </div>
     {#each $filteredList as game}
         <BoardGame game={game} />
     {:else}
         No games found
     {/each}
 </div>
+
+<style>
+    .filters {
+        margin-bottom: 2rem;
+    }
+</style>
